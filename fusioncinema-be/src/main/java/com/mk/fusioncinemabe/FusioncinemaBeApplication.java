81e11bfc56e9f6b0dac8db0cba6031624da576cb
Dario <106852390+Minkaspr@users.noreply.github.com>
2024-03-08 11:31:33 -0500

    initial project

---
+++ fusioncinema-be/src/main/java/com/mk/fusioncinemabe/FusioncinemaBeApplication.java
@@ -0,0 +1,13 @@
+package com.mk.fusioncinemabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusioncinemaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusioncinemaBeApplication.class, args);
+	}
+
+}
---
+++ fusioncinema-be/src/test/java/com/mk/fusioncinemabe/FusioncinemaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.fusioncinemabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusioncinemaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
